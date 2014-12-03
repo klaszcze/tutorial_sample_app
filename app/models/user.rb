2c2ed2b1f225af0e7fc14e6f5db1ac598d12a769
Kasia <Katarzyna.laszczewska@gmail.com>
2014-12-03 22:13:57 +0100

    level 6 exercizes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	before_save { self.email = email.downcase }
+	before_save { self.email = email.downcase! }
 	validates :name, presence: true, length: {maximum: 50}
 	validates :email, presence: true, length: {maximum: 255}, uniqueness: {case_sensitive: false}
 	validates :email, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
